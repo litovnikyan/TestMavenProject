acd8450086e550b491435d15298b3260e9919031
Yanis <yanis.litovnik@yandex.ru>
2023-11-15 13:31:09 +0300

    Третий коммит

--- lessons/src/main/java/com/max/App.java
+++ lessons/src/main/java/com/max/App.java
@@ -12,8 +12,10 @@ public class App
 
         Commit commit = new Commit("Тестируем первый коммит", 1);
         Commit commit2 = new Commit("Commit 2", 2);
+        Commit commit3 = new Commit("Commit 3", 3);
 
         System.out.println(commit);
         System.out.println(commit2);
+        System.out.println(commit3);
     }
 }
