5467554afa460887fd44fa1e1c276146f29d2301
Yanis <yanis.litovnik@yandex.ru>
2023-11-15 13:16:48 +0300

    Добавили 2 коммит

--- lessons/src/main/java/com/max/App.java
+++ lessons/src/main/java/com/max/App.java
@@ -9,7 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
         Commit commit = new Commit("Тестируем первый коммит", 1);
+        Commit commit2 = new Commit("Commit 2", 2);
+
         System.out.println(commit);
+        System.out.println(commit2);
     }
 }
